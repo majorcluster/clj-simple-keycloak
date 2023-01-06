9c2b36d932d9b6bf5babda5c4cd030aec5a55df4
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-01-06 18:44:38 -0300

    Bumped deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.majorcluster/clj-simple-keycloak "0.1.0"
+(defproject org.clojars.majorcluster/clj-simple-keycloak "0.2.0"
   :description "A clojure library to simply resolve authentication and authorization on an externally maintained keycloak server"
   :url "https://github.com/mtsbarbosa/clj-simple-keycloak"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [http-kit "2.7.0-alpha1"]
-                 [org.clojure/data.json "0.2.6"]
-                 [nubank/matcher-combinators "1.2.1"]]
+                 [org.clojure/data.json "2.4.0"]
+                 [nubank/matcher-combinators "3.7.2"]]
   :repl-options {:init-ns clj-simple-keycloak.core})
