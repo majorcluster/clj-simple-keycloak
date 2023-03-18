e523c58a017664f41c0b40d3a598016ebe5d114a
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:50:19 -0300

    Adjusting github url

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.majorcluster/clj-simple-keycloak "0.2.0"
+(defproject org.clojars.majorcluster/clj-simple-keycloak "0.2.1"
   :description "A clojure library to simply resolve authentication and authorization on an externally maintained keycloak server"
-  :url "https://github.com/mtsbarbosa/clj-simple-keycloak"
+  :url "https://github.com/majorcluster/clj-simple-keycloak"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [http-kit "2.7.0-alpha1"]
                  [org.clojure/data.json "2.4.0"]
-                 [nubank/matcher-combinators "3.7.2"]]
+                 [nubank/matcher-combinators "3.8.4"]]
   :repl-options {:init-ns clj-simple-keycloak.core})
